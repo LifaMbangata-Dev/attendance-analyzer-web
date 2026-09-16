90a1bec92358fe5c06f730ebf2352045f6e52b8a
hloni <hloniyacho@gmial.com>
2026-09-16 22:48:57 +0200

    Update production API URL

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080/api';
-
+//const API_BASE_URL = 'http://localhost:8080/api';
+const API_BASE_URL = 'https://attendance-analyzer-production-9958.up.railway.app/api';
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
